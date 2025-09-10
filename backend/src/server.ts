94b1a614797641b0ec874c3aa00276bf9bdb84c2
Gerlandio Bernardo <gerlandios51@gmail.com>
2025-09-10 14:57:38 -0300

    feat: implementar rota para cadastro e login do usuario

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import rootRouter from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', rootRouter);
 
 app.listen(port, ()=> {
     console.log(`Server running on port ${port}`);
